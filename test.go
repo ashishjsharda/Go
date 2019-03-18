81d233c7a39231c52ccf3e742c0bec5e3593a75f
Ashish Sharda <asharda@tree.com>
2019-03-18 06:59:21 -0400

    Add all my files

---
+++ test.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var text="This is sample test" //sample string
+	fmt.Printf("Hello World \n")
+	fmt.Printf("Greeting seen is :")
+	fmt.Printf("%s",text)
+
+}
