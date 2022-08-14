bb6477431a27c4a80c0aaeeddde2eeb654163a78
Ashish S <ashishjsharda@gmail.com>
2022-08-14 14:43:16 -0500

    Update basicapi.go

--- basicapi.go
+++ basicapi.go
@@ -6,12 +6,16 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome !")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func greeting(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome !!")
+	fmt.Println("Endpoint Hit: greeting")
+}
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", greeting)
 	http.ListenAndServe(":8080", nil)
 }
 
